8ed58c00d934290821a1608655e0bdcdf3eaa9b9
DavidAyad97 <davidayad80@gmail.com>
2024-04-20 18:13:08 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'swift-cart';
+  items:MenuItem[] | undefined;
+  ngOnInit(): void {
+      this.items = [
+        {
+          label:'file',
+          items:[
+            {
+              label:'new',
+              icon:'pi pi-fw pi-plus',
+              items:[
+                {label:'project',url:'https://github.com'},
+                {label:'products',routerLink:['products']}
+              ]
+            }
+          ]
+        },{
+          label:'Edit',
+          items:[
+            {label:'update', command:()=>this.log()},
+            {label:'delete'}
+          ],
+          style:{
+            color:'red'
+          }
+        }
+      ]
+  }
+  
+  log(){
+    console.log("Helllllo");
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
